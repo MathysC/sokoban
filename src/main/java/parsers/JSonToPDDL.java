7a3dc349127a63fbdeab2f5e73c339160cb1e2f4
Maleusa <yazid.cheriti@gmail.comgit config --global user.email yazid.cheriti@gmail.com>
2023-03-10 11:45:58 +0100

    Feat : JSONtoPDDL parser

--- src/main/java/parsers/JSonToPDDL.java
+++ src/main/java/parsers/JSonToPDDL.java
@@ -18,6 +18,10 @@ private boolean isTest;
 private boolean isValidator;
 private String mapModel;
 private FileWriter writer;
+private ArrayList<String> floorPlan;
+private ArrayList<String> goal;
+private ArrayList<String> boxes;
+private String guardPosition;
 
 public JSonToPDDL(String pathToFile) {
 	parser = new JSONParser();
@@ -25,7 +29,9 @@ public JSonToPDDL(String pathToFile) {
 	try {
 		reader = new FileReader(pathToFile);
 		JSONObject temp = (JSONObject ) parser.parse(reader);
-		
+		floorPlan= new ArrayList<String> ();
+		goal=new ArrayList<String>();
+		boxes=new ArrayList<String>();
 		mapName= temp.get("title").toString();
 		mapModel = temp.get("testIn").toString();
 		isTest= (temp.get("isTest").equals("true"));
@@ -38,10 +44,56 @@ public JSonToPDDL(String pathToFile) {
 	System.out.println(mapName+mapModel);
 }
 
-public void createPDDL() {
+public void createHeader() throws IOException {
 	ArrayList<String> map = new ArrayList<String>(Arrays.asList(mapModel.split("\n")));
 	writer.append("(define ( problem "+mapName + " )");
-	writer.append("(:domain SOKOBAN)");
+	writer.append("(:domain SOKOBAN)\n");
+	writer.append("(:objects\n");
+}
+public void createObjects() throws IOException{
+	boolean betweenWalls=false;
+	int row=1;
+	int cell=1;
+	int destNumber=1;
+	char[] arrayMap=new char[mapModel.length()];
+	mapModel.getChars(0, mapModel.length(), arrayMap, 0);
+	for (int i =0 ; i<mapModel.length(); i++) {
+		if(arrayMap[i]=='#') 
+			betweenWalls=!betweenWalls;
+		if (betweenWalls) {
+		switch (arrayMap[i])
+		{
+		case ' ':
+			writer.append("f"+row+cell+"- floor\n");
+			floorPlan.add("f"+row+cell);
+			cell++;
+			break;
+		case '.':
+			writer.append("f"+row+cell+"- floor\n");
+			floorPlan.add("f"+row+cell);
+			goal.add("f"+row+cell+" ");
+			writer.append("Dest"+destNumber+"- destination\n");
+			destNumber++;
+			cell++;
+			break;
+		case '@':
+			writer.append("f"+row+cell+"- floor\n");
+			guardPosition="f"+row+cell;
+			cell++;
+			break;
+		}
+		}
+		
+	}
+}
+
+public void createPDDL() {
+	try {
+		createHeader();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	
 	
 }
